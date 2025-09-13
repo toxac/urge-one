2f91564b0e3b1ba98308e00ff06f5d683c2980ca
toxac <derocon@gmail.com>
2025-09-13 21:01:30 +0530

    added app types

---
+++ types/urgeTypes.ts
@@ -0,0 +1,18 @@
+import type { User, Session } from '@supabase/supabase-js'
+
+// ------ Authentication and Supabase ----------
+
+export interface AuthStore {
+    user: User | null
+    session: Session | null
+    loading: boolean
+}
+
+export type AuthEventType = 'INITIAL_SESSION' |'SIGNED_IN' | 'SIGNED_OUT' | 'TOKEN_REFRESHED' | 'USER_UPDATED' | 'PASSWORD_RECOVERY'
+
+export interface SupabaseCustomError {
+    name: string;
+    message: string;
+    details?: string
+}
+
