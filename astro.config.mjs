cd363afde60603ff055748d36adb141ff3f43973
toxac <derocon@gmail.com>
2025-09-13 18:13:38 +0530

    setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import vercel from '@astrojs/vercel';
+
+import solidJs from '@astrojs/solid-js';
+
+import sitemap from '@astrojs/sitemap';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: vercel(),
+  integrations: [solidJs(), sitemap(), mdx()]
+});
