bcd366a3038b7444e52904a875834c96fbc93394
toxac <derocon@gmail.com>
2025-10-13 18:14:48 +0530

    testing editing modes

--- src/components/user/assets/OpportunityListModed.tsx
+++ src/components/user/assets/OpportunityListModed.tsx
@@ -15,65 +15,91 @@ import type { Database } from "../../../../database.types";
 
 type Opportunity = Database['public']['Tables']['user_opportunities']['Row'];
 
-type ActionMode = 
-  | "editExploration" 
-  | "editCustomerResearch" 
-  | "editMarketSize" 
-  | "editAssessment" 
-  | "editAll" 
-  | "viewOnly";
+type ActionMode =
+    | "editExploration"
+    | "editCustomerResearch"
+    | "editMarketSize"
+    | "editAssessment"
+    | "editAll"
+    | "viewOnly";
 
 interface OpportunitiesListProps {
-  mode?: ActionMode;
-  title?: string;
-  description?: string;
+    mode: ActionMode;
 }
 
 export default function OpportunitiesList(props: OpportunitiesListProps) {
-  const mode = () => props.mode || "viewOnly";
-  
-  const $opportunities = useStore(opportunitiesStore);
-  const $opportunitiesLoading = useStore(opportunitiesStoreLoading);
+    const mode = () => props.mode || "viewOnly";
 
-  const [showAddModal, setShowAddModal] = createSignal(false);
-  const [showExplorationModal, setShowExplorationModal] = createSignal(false);
-  const [showCustomerResearchModal, setShowCustomerResearchModal] = createSignal(false);
-  const [showMarketSizeModal, setShowMarketSizeModal] = createSignal(false);
-  const [showAssessmentModal, setShowAssessmentModal] = createSignal(false);
-  
-  const [selectedOpportunity, setSelectedOpportunity] = createSignal<Opportunity | null>(null);
-  const [savedOpportunities, setSavedOpportunities] = createSignal<Opportunity[] | []>([]);
-  const [loading, setLoading] = createSignal(false);
-  
+    const $opportunities = useStore(opportunitiesStore);
+    const $opportunitiesLoading = useStore(opportunitiesStoreLoading);
+
+    const [showAddModal, setShowAddModal] = createSignal(false);
+    const [showEditModal, setShowEditModal] = createSignal(false);
+
+    const [selectedOpportunity, setSelectedOpportunity] = createSignal<Opportunity | null>(null);
+    const [savedOpportunities, setSavedOpportunities] = createSignal<Opportunity[] | []>([]);
+    const [loading, setLoading] = createSignal(false);
 
 
-  createEffect(() => {
-    if ($opportunitiesLoading()) {
-      setLoading(true);
-      return;
-    } else {
-      const opportunities = $opportunities();
-      setSavedOpportunities(opportunities);
-      setLoading(false);
+
+    createEffect(() => {
+        if ($opportunitiesLoading()) {
+            setLoading(true);
+            return;
+        } else {
+            const opportunities = $opportunities();
+            setSavedOpportunities(opportunities);
+            setLoading(false);
+        }
+    });
+
+    const handleSelectOpportunity = (currentOpportunity: Opportunity) => {
+        setSelectedOpportunity(currentOpportunity)
+        if(props.mode === "editAll"){
+            // navigate to edit page its better to open that up in separate page as there are so many fields
+        } else {
+            setShowEditModal(true);
+        }
     }
-  });
+
+    const handleEditSuccess = () =>{
+        setShowEditModal(false);
+    }
 
 
 
 
 
-  return (
-    <div>
-        <For each={savedOpportunities()}>
-            {opportunity => {
-                return (
-                    <div>
-                        {opportunity.title}
-                    </div>
-                )
-            }}
-        </For>
+    return (
+        <div>
+            <For each={savedOpportunities()}>
+                {opportunity => {
+                    return (
+                        <div class="flex">
+                            <h4>{opportunity.title}</h4>
+                            <button class="btn btn-primary btn-outline" onClick={() => handleSelectOpportunity(opportunity)}>Edit</button>
+                        </div>
+                    )
+                }}
+            </For>
 
-    </div>
-  );
+            <Modal isOpen={props.mode == "editExploration" && showEditModal()} onClose={()=> setShowEditModal(false)}>
+                <div>Editing Market Exploration for {selectedOpportunity()?.title}</div>
+            </Modal>
+            <Modal isOpen={props.mode == "editCustomerResearch" && showEditModal()} onClose={()=> setShowEditModal(false)}>
+                <div>Editing Customer Research for {selectedOpportunity()?.title}</div>
+            </Modal>
+            <Modal isOpen={props.mode == "editMarketSize" && showEditModal()} onClose={()=> setShowEditModal(false)}>
+                <div>Editing Market Size for {selectedOpportunity()?.title}</div>
+            </Modal>
+            <Modal isOpen={props.mode == "editAssessment" && showEditModal()} onClose={()=> setShowEditModal(false)}>
+                <div>Editing Assessment for {selectedOpportunity()?.title}</div>
+            </Modal>
+
+
+
+
+
+        </div>
+    );
 }
